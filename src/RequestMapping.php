feb71394418439e117dfd19913bc21490f6faaf7
ctocode <343196936@qq.com>
2021-06-25 16:19:34 +0800

    初始化版本

---
+++ src/RequestMapping.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace shiyun\annotation;
+
+/**
+ * 注册路由
+ * @package topthink\annotation
+ * @Annotation
+ * @Target({"METHOD","CLASS"})
+ */
+final class RequestMapping
+{
+    /**
+     * @Required()
+     * @var string
+     */
+    public $path;
+
+    /**
+     * @Required()
+     * @var string
+     */
+    public $value;
+
+
+    // /**
+    //  * @Enum({"POST", "GET", "PUT", "DELETE"})
+    //  * @var string
+    //  */
+
+
+    /**
+     * 请求类型
+     * @Enum({"GET","POST","PUT","DELETE","PATCH","OPTIONS","HEAD"})
+     * @var string
+     */
+    public $method = "*";
+
+    /**
+     * @var array
+     */
+    public $param;
+
+    public $time;
+}
